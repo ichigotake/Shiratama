ca5dacfce34c45d047833bf37bec2b655afd6667
ichigotake <k.wisiiy@gmail.com>
2013-06-16 15:09:57 +0900

    change view directory

--- lib/Shiratama/Constant.php
+++ lib/Shiratama/Constant.php
@@ -4,5 +4,5 @@ define('SHIRATAMA_VERSION', Shiratama::$VERSION);
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(__DIR__ . DS . '..' . DS . '..'));
 define('APP_ROOT', ROOT . DS . 'app');
-define('APP_VIEW_DIR', APP_ROOT . DS . 'View');
+define('APP_VIEW_DIR', APP_ROOT);
 
