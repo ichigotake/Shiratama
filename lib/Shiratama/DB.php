7e14b5f9a5a7e6b46c6924eb8b7d0fe610637e09
ichigotake <k.wisiiy@gmail.com>
2013-07-02 01:27:25 +0900

    Shiratama_DB extends Model

--- lib/Shiratama/DB.php
+++ lib/Shiratama/DB.php
@@ -1,11 +1,12 @@
 <?php
 
-class Shiratama_DB
+class Shiratama_DB extends Shiratama_Model
 {
     public function __construct($config)
     {
         $this->dbh = new PDO($config[0], $config[1], $config[2]);
         $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        parent::__construct(array('driver' => 'mysql', 'dbh' => $this->dbh));
     }
 
 }
